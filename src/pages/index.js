8daa3940eb04ef37b418a0861fed195d77ed0440
xxbloodcor3xx <xx_bloodcor3_xx@outlook.com>
2025-03-04 15:20:07 -0800

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Image, Text, LinkBox, Box, Section, Strong } from "@quarkly/widgets";
+import { Theme, Link, Button, Text, Strong, Section, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
-import { RawHtml, Override, SocialMedia } from "@quarkly/components";
-import * as Components from "components";
-import { FiMenu } from "react-icons/fi";
-import { AiOutlineMenu } from "react-icons/ai";
+import { RawHtml, Override } from "@quarkly/components";
 export default (() => {
 	return <Theme theme={theme}>
 		<GlobalQuarklyPageStyles pageUrl={"index"} />
@@ -17,297 +14,19 @@ export default (() => {
 			<meta name={"description"} content={"Web site created using quarkly.io"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
-		<Section sm-padding="8px 0 8px 0" quarkly-title="Header-6" padding="24px 0px 24px 0px">
-			<Override
-				slot="SectionContent"
-				flex-direction="row"
-				justify-content="space-between"
-				max-width="100%"
-				width="100%"
-				padding="0px 24px 0px 24px"
-			/>
-			<Components.QuarklycommunityKitMobileSidePanel
-				menuPosition="full"
-				breakpoint="lg"
-				width="40%"
-				sm-width="50%"
-				md-width="50%"
-				lg-width="40%"
-				lg-justify-content="flex-start"
-				md-justify-content="flex-end"
-			>
-				<Override slot="Children" md-display="flex" />
-				<Override
-					slot="Content"
-					padding="0px 0px 0px 0px"
-					background="rgba(255, 255, 255, 0)"
-					lg-background="#ffffff"
-					lg-margin="0px 0px 0px 0px"
-				/>
-				<Override
-					slot="Button Text"
-					font="normal 600 16px/1.5 -apple-system, system-ui, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"
-					text-transform="uppercase"
-					letter-spacing="1px"
-					sm-font="normal 600 14px/1.5 -apple-system, system-ui, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif"
-					sm-margin="0px 2px 0px 0px"
-					lg-margin="0px 0px 0px 0px"
-				/>
-				<Override
-					slot="Button Icon :closed"
-					category="fi"
-					icon={FiMenu}
-					size="32px"
-					padding="5px 7px 5px 7px"
-					border-radius="50px"
-				/>
-				<Override
-					slot="Button Icon"
-					width="28px"
-					height="28px"
-					category="ai"
-					icon={AiOutlineMenu}
-					color="--dark"
-					size="24px"
-					lg-width="32px"
-					lg-height="32px"
-				/>
-				<Override
-					slot="Cross"
-					lg-width="32px"
-					lg-height="32px"
-					size="32px"
-					top="24px"
-					right="24px"
-				/>
-				<Box
-					align-items="center"
-					lg-justify-content="center"
-					lg-align-items="flex-start"
-					justify-content="flex-start"
-					display="flex"
-					lg-flex-direction="column"
-					lg-margin="0px auto 0px auto"
-					lg-min-width="300px"
-					lg-max-width="1280px"
-					lg-width="90%"
-					lg-padding="24px 0px 48px 0px"
-				>
-					<Box
-						display="none"
-						lg-width="100%"
-						lg-margin="0px 0px 24px 0px"
-						lg-display="flex"
-						lg-padding="12px 0px 12px 0px"
-					>
-						<LinkBox flex-direction="row" href="/index" display="flex" grid-gap="12px">
-							<Image src="https://uploads.quarkly.io/6457b971d28fe100213a0f35/images/flower-logo-template.svg?v=2023-08-06T19:34:51.878Z" display="block" width="36px" height="36px" />
-							<Text
-								margin="0"
-								md-margin="0px 0 0px 0"
-								text-align="left"
-								font="--lead"
-								sm-margin="0px 0 0px 0"
-								display="block"
-								lg-font="--headline3"
-							>
-								Company
-							</Text>
-						</LinkBox>
-					</Box>
-					<Components.QuarklycommunityKitMenu
-						display="flex"
-						filterMode="exclude"
-						filterPages="/index"
-						grid-gap="4px"
-						lg-flex-direction="column"
-						lg-padding="6px 0px 6px 0px"
-						lg-margin="0px 0px 24px 0px"
-						align-items="center"
-						flex-wrap="wrap"
-						overflow-x="visible"
-						overflow-y="visible"
-						lg-align-items="flex-start"
-					>
-						<Override
-							slot="link"
-							color="--darkL2"
-							hover-color="--primary"
-							font="--base"
-							text-decoration-line="initial"
-							transition="color 0.1s ease 0s"
-							lg-font="--lead"
-						/>
-						<Override
-							slot="item-active"
-							border-width="0 0 1px 0"
-							border-style="solid"
-							border-color="--color-darkL2"
-							lg-border-width="0 0 0 2px"
-						/>
-						<Override slot="item" padding="6px 6px 6px 6px" lg-padding="6px 12px 6px 12px" />
-						<Override slot="link-active" cursor="default" color="--darkL2" hover-color="--darkL2" />
-						<Override slot="link-about" />
-					</Components.QuarklycommunityKitMenu>
-					<Box
-						display="none"
-						padding="12px 0"
-						justify-content="flex-end"
-						align-items="flex-start"
-						flex-direction="row"
-						width="40%"
-						lg-width="100%"
-						md-justify-content="flex-start"
-						lg-justify-content="flex-start"
-						lg-padding="0px 0 12px 0"
-						lg-flex-direction="column"
-						lg-margin="0px 0px 24px 0px"
-						lg-display="flex"
-						lg-grid-gap="24px"
-					>
-						<Link
-							border-radius="8px"
-							href="#"
-							text-decoration-line="initial"
-							border-style="solid"
-							font="--base"
-							color="--darkL1"
-							margin="0px 0px 0px 34px"
-							border-width="2px"
-							padding="4px 12px 5px 12px"
-							border-color="--color-darkL1"
-							lg-margin="0px 0px 0px 0px"
-							lg-padding="10px 16px 10px 16px"
-							lg-font="--lead"
-						>
-							Sign Up
-						</Link>
-						<Link
-							font="--base"
-							text-decoration-line="initial"
-							margin="0px 0px 0px 16px"
-							background="--color-darkL1"
-							border-radius="8px"
-							href="#"
-							color="#ffffff"
-							padding="6px 12px 7px 12px"
-							lg-padding="12px 16px 12px 16px"
-							lg-font="--lead"
-							lg-margin="0px 0px 0px 0px"
-						>
-							Book A Demo
-						</Link>
-					</Box>
-					<Box
-						width="25%"
-						display="none"
-						lg-width="100%"
-						lg-flex-direction="column"
-						lg-align-items="flex-start"
-						lg-display="flex"
-						justify-content="space-around"
-						align-items="center"
-						flex-wrap="wrap"
-						lg-margin="24px 0px 0px 0px"
-					>
-						<box>
-							</Override>
-							<Override
-								slot="link"
-								border-radius="50%"
-								background="transparent"
-								hover-color="--light"
-								display="flex"
-								margin="0 5px 0 5px"
-								padding="5x 5px 5px 5px"
-								width="32px"
-								height="32px"
-								align-items="center"
-								justify-content="center"
-							/>
-							<Override slot="icon" size="32px" border-radius="50px" color="--grey" />
-						
-			</Components.QuarklycommunityKitMobileSidePanel>
-			<Box
-				display="flex"
-				padding="12px 0"
-				justify-content="center"
-				align-items="flex-start"
-				flex-direction="row"
-				width="20%"
-				sm-width="50%"
-				sm-align-items="center"
-				sm-flex-direction="row"
-				sm-justify-content="flex-start"
-				md-width="50%"
-				lg-width="20%"
-				md-justify-content="flex-start"
-				md-order="-1"
-			>
-				<LinkBox flex-direction="row" href="/index" display="flex" grid-gap="12px">
-					<Image src="https://uploads.quarkly.io/6457b971d28fe100213a0f35/images/flower-logo-template.svg?v=2023-08-06T19:34:51.878Z" display="block" width="36px" height="36px" />
-					<Text
-						margin="0"
-						md-margin="0px 0 0px 0"
-						text-align="left"
-						font="--lead"
-						sm-margin="0px 0 0px 0"
-						display="block"
-					>
-						Company
-					</Text>
-				</LinkBox>
-			</Box>
-			<Box
-				display="flex"
-				padding="12px 0"
-				justify-content="flex-end"
-				align-items="flex-start"
-				flex-direction="row"
-				width="40%"
-				sm-width="50%"
-				sm-align-items="center"
-				sm-flex-direction="row"
-				sm-justify-content="flex-start"
-				md-width="50%"
-				lg-width="40%"
-				md-justify-content="flex-start"
-				md-display="none"
-			>
-				<Link
-					border-radius="8px"
-					href="#"
-					text-decoration-line="initial"
-					border-style="solid"
-					sm-padding="5px 10px 5px 10px"
-					md-padding="5px 10px 5px 10px"
-					sm-margin="10px 0px 10px 0px"
-					font="--base"
-					color="--darkL1"
-					margin="0px 0px 0px 34px"
-					md-margin="10px 0px 0px 0px"
-					border-width="2px"
-					padding="4px 12px 5px 12px"
-					border-color="--color-darkL1"
-				>
-					Apply
-				</Link>
-				<Link
-					font="--base"
-					text-decoration-line="initial"
-					margin="0px 0px 0px 16px"
-					background="--color-darkL1"
-					border-radius="8px"
-					md-margin="0px 0px 0px 14px"
-					href="#"
-					color="#ffffff"
-					padding="6px 12px 7px 12px"
-					md-order="1"
-				>
-					Clients
-				</Link>
-			</Box>
-		</Section>
+		<Button
+			position="fixed"
+			top="49px"
+			left="auto"
+			z-index="2"
+			href="/"
+			bottom="auto"
+			height="auto"
+			right="271px"
+			width="auto"
+		>
+			Clients
+		</Button>
 		<Section
 			padding="60px 0 60px 0"
 			sm-padding="40px 0 40px 0"
@@ -390,6 +109,20 @@ export default (() => {
 				We accept any artist as long as they have a minimum of one release per month. Any artist from any genre works! If you're a part of a collective, that is perfectly fine! Being signed to any PRO's is okay as well, as long as you provide your IPI number.
 			</Text>
 		</Section>
+		<Button
+			position="fixed"
+			top="49px"
+			left="auto"
+			z-index="2"
+			href="/"
+			bottom="auto"
+			height="auto"
+			right="158px"
+			width="auto"
+			type="link"
+		>
+			Apply
+		</Button>
 		<Section padding="80px 0 80px 0" sm-padding="60px 0 60px 0" background="--color-dark">
 			<Override slot="SectionContent" flex-direction="row" md-flex-wrap="wrap" />
 			<Box
@@ -708,6 +441,10 @@ export default (() => {
 			transition={"--opacityOut"}
 			quarkly-title={"Badge"}
 			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
 		</Link>
 		<RawHtml>
 			<style place={"endOfHead"} rawKey={"67c737959335410018cacb82"}>
