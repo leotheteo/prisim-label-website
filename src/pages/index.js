e6d62e409b571d286aaf3a20cfb0b3129ad49777
xxbloodcor3xx <xx_bloodcor3_xx@outlook.com>
2025-03-04 15:03:14 -0800

    Changes changed header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Link, Image, Text, LinkBox, Box, Button, Section, Strong } from "@quarkly/widgets";
+import { Theme, Link, Image, Text, LinkBox, Box, Section, Strong } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
 import { RawHtml, Override, SocialMedia } from "@quarkly/components";
@@ -17,61 +17,24 @@ export default (() => {
 			<meta name={"description"} content={"Web site created using quarkly.io"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
-		<Section sm-padding="8px 0 8px 0" quarkly-title="Header-4" width="auto" height="auto">
+		<Section sm-padding="8px 0 8px 0" quarkly-title="Header-6" padding="24px 0px 24px 0px">
 			<Override
 				slot="SectionContent"
 				flex-direction="row"
 				justify-content="space-between"
-				width="auto"
-				height="auto"
+				max-width="100%"
+				width="100%"
+				padding="0px 24px 0px 24px"
 			/>
-			<Box
-				display="flex"
-				padding="12px 0"
-				justify-content="space-between"
-				align-items="flex-start"
-				flex-direction="row"
-				width="auto"
-				sm-width="50%"
-				sm-align-items="center"
-				sm-flex-direction="row"
-				sm-justify-content="flex-start"
-				md-width="50%"
-				lg-width="70%"
-				md-justify-content="flex-start"
-				height="auto"
-			>
-				<LinkBox flex-direction="row" href="/index" display="flex" grid-gap="12px">
-					<Image
-						src="https://uploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17:34:40.476Z"
-						display="block"
-						width="36px"
-						height="36px"
-						srcSet="https://smartuploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17%3A34%3A40.476Z&quality=85&w=500 500w,https://smartuploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17%3A34%3A40.476Z&quality=85&w=800 800w,https://smartuploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17%3A34%3A40.476Z&quality=85&w=1080 1080w,https://smartuploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17%3A34%3A40.476Z&quality=85&w=1600 1600w,https://smartuploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17%3A34%3A40.476Z&quality=85&w=2000 2000w,https://smartuploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17%3A34%3A40.476Z&quality=85&w=2600 2600w,https://smartuploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17%3A34%3A40.476Z&quality=85&w=3200 3200w"
-						sizes="(max-width: 576px) 100vw,(max-width: 768px) 100vw,(max-width: 992px) 100vw,100vw"
-					/>
-					<Text
-						margin="0"
-						md-margin="0px 0 0px 0"
-						text-align="left"
-						font="--headline3"
-						sm-margin="0px 0 0px 0"
-						display="block"
-						width="auto"
-						height="auto"
-					>
-						Prism Audio{"\n\n"}
-					</Text>
-				</LinkBox>
-			</Box>
 			<Components.QuarklycommunityKitMobileSidePanel
 				menuPosition="full"
 				breakpoint="lg"
-				width="auto"
+				width="40%"
 				sm-width="50%"
 				md-width="50%"
-				lg-width="30%"
-				height="auto"
+				lg-width="40%"
+				lg-justify-content="flex-start"
+				md-justify-content="flex-end"
 			>
 				<Override slot="Children" md-display="flex" />
 				<Override
@@ -117,18 +80,11 @@ export default (() => {
 					top="24px"
 					right="24px"
 				/>
-				<Override slot="Wrapper" width="auto" height="auto" />
-				<Button type="link" href="https://docs.google.com/forms/d/e/1FAIpQLSeqxt2vzCF_F90cRN2j9tRbuOfIq2Eu6n1BJurDvkgJGNqxbg/viewform?usp=dialog" width="auto" height="auto">
-					Apply
-				</Button>
-				<Button type="link" href="https://docs.google.com/forms/d/e/1FAIpQLScb0hygBn_AMHkJbDxHyqWkv8NiiX1dH2Kl_oPU50eMyJJ59Q/viewform?usp=dialog" width="auto" height="auto">
-					For Clients
-				</Button>
 				<Box
 					align-items="center"
 					lg-justify-content="center"
 					lg-align-items="flex-start"
-					justify-content="flex-end"
+					justify-content="flex-start"
 					display="flex"
 					lg-flex-direction="column"
 					lg-margin="0px auto 0px auto"
@@ -163,7 +119,7 @@ export default (() => {
 						display="flex"
 						filterMode="exclude"
 						filterPages="/index"
-						grid-gap="8px"
+						grid-gap="4px"
 						lg-flex-direction="column"
 						lg-padding="6px 0px 6px 0px"
 						lg-margin="0px 0px 24px 0px"
@@ -177,22 +133,71 @@ export default (() => {
 							slot="link"
 							color="--darkL2"
 							hover-color="--primary"
-							font="--lead"
+							font="--base"
 							text-decoration-line="initial"
 							transition="color 0.1s ease 0s"
 							lg-font="--lead"
 						/>
 						<Override
 							slot="item-active"
-							border-width="0 0 2px 0"
+							border-width="0 0 1px 0"
 							border-style="solid"
 							border-color="--color-darkL2"
 							lg-border-width="0 0 0 2px"
 						/>
-						<Override slot="item" padding="6px 8px 6px 8px" lg-padding="6px 12px 6px 12px" />
+						<Override slot="item" padding="6px 6px 6px 6px" lg-padding="6px 12px 6px 12px" />
 						<Override slot="link-active" cursor="default" color="--darkL2" hover-color="--darkL2" />
 						<Override slot="link-about" />
 					</Components.QuarklycommunityKitMenu>
+					<Box
+						display="none"
+						padding="12px 0"
+						justify-content="flex-end"
+						align-items="flex-start"
+						flex-direction="row"
+						width="40%"
+						lg-width="100%"
+						md-justify-content="flex-start"
+						lg-justify-content="flex-start"
+						lg-padding="0px 0 12px 0"
+						lg-flex-direction="column"
+						lg-margin="0px 0px 24px 0px"
+						lg-display="flex"
+						lg-grid-gap="24px"
+					>
+						<Link
+							border-radius="8px"
+							href="#"
+							text-decoration-line="initial"
+							border-style="solid"
+							font="--base"
+							color="--darkL1"
+							margin="0px 0px 0px 34px"
+							border-width="2px"
+							padding="4px 12px 5px 12px"
+							border-color="--color-darkL1"
+							lg-margin="0px 0px 0px 0px"
+							lg-padding="10px 16px 10px 16px"
+							lg-font="--lead"
+						>
+							Sign Up
+						</Link>
+						<Link
+							font="--base"
+							text-decoration-line="initial"
+							margin="0px 0px 0px 16px"
+							background="--color-darkL1"
+							border-radius="8px"
+							href="#"
+							color="#ffffff"
+							padding="6px 12px 7px 12px"
+							lg-padding="12px 16px 12px 16px"
+							lg-font="--lead"
+							lg-margin="0px 0px 0px 0px"
+						>
+							Book A Demo
+						</Link>
+					</Box>
 					<Box
 						width="25%"
 						display="none"
@@ -203,12 +208,119 @@ export default (() => {
 						justify-content="space-around"
 						align-items="center"
 						flex-wrap="wrap"
-						lg-margin="0px 0px 0px 0px"
+						lg-margin="24px 0px 0px 0px"
 					>
-					
+						<SocialMedia
+							instagram="https://instagram.com/instagram"
+							margin="0px 0px 0px 0px"
+							facebook="https://www.facebook.com/quarklyapp/"
+							youtube="https://www.youtube.com/channel/UCK5bXs2L0bbSMQ82BQ3hIkw"
+							lg-display="flex"
+							lg-grid-gap="12px"
+						>
+							<Override slot="link-twitter" margin="0px 0px 0px 5px">
+								<div />
+							</Override>
+							<Override
+								slot="link"
+								border-radius="50%"
+								background="transparent"
+								hover-color="--light"
+								display="flex"
+								margin="0 5px 0 5px"
+								padding="5x 5px 5px 5px"
+								width="32px"
+								height="32px"
+								align-items="center"
+								justify-content="center"
+							/>
+							<Override slot="icon" size="32px" border-radius="50px" color="--grey" />
+							<Override slot="link-facebook" margin="0px 5px 0px 0px">
+								<div />
+							</Override>
+						</SocialMedia>
 					</Box>
 				</Box>
 			</Components.QuarklycommunityKitMobileSidePanel>
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="center"
+				align-items="flex-start"
+				flex-direction="row"
+				width="20%"
+				sm-width="50%"
+				sm-align-items="center"
+				sm-flex-direction="row"
+				sm-justify-content="flex-start"
+				md-width="50%"
+				lg-width="20%"
+				md-justify-content="flex-start"
+				md-order="-1"
+			>
+				<LinkBox flex-direction="row" href="/index" display="flex" grid-gap="12px">
+					<Image src="https://uploads.quarkly.io/6457b971d28fe100213a0f35/images/flower-logo-template.svg?v=2023-08-06T19:34:51.878Z" display="block" width="36px" height="36px" />
+					<Text
+						margin="0"
+						md-margin="0px 0 0px 0"
+						text-align="left"
+						font="--lead"
+						sm-margin="0px 0 0px 0"
+						display="block"
+					>
+						Company
+					</Text>
+				</LinkBox>
+			</Box>
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="flex-end"
+				align-items="flex-start"
+				flex-direction="row"
+				width="40%"
+				sm-width="50%"
+				sm-align-items="center"
+				sm-flex-direction="row"
+				sm-justify-content="flex-start"
+				md-width="50%"
+				lg-width="40%"
+				md-justify-content="flex-start"
+				md-display="none"
+			>
+				<Link
+					border-radius="8px"
+					href="#"
+					text-decoration-line="initial"
+					border-style="solid"
+					sm-padding="5px 10px 5px 10px"
+					md-padding="5px 10px 5px 10px"
+					sm-margin="10px 0px 10px 0px"
+					font="--base"
+					color="--darkL1"
+					margin="0px 0px 0px 34px"
+					md-margin="10px 0px 0px 0px"
+					border-width="2px"
+					padding="4px 12px 5px 12px"
+					border-color="--color-darkL1"
+				>
+					Apply
+				</Link>
+				<Link
+					font="--base"
+					text-decoration-line="initial"
+					margin="0px 0px 0px 16px"
+					background="--color-darkL1"
+					border-radius="8px"
+					md-margin="0px 0px 0px 14px"
+					href="#"
+					color="#ffffff"
+					padding="6px 12px 7px 12px"
+					md-order="1"
+				>
+					Clients
+				</Link>
+			</Box>
 		</Section>
 		<Section
 			padding="60px 0 60px 0"
