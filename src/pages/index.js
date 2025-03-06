4dc424aeec68af20ac446af3f2655e9d685503aa
leotheteo <xx_bloodcor3_xx@outlook.com>
2025-03-06 10:14:33 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -322,12 +322,7 @@ const dsps = [
 ];
 
 export default (() => {
-    return (
-        <Theme theme={theme}>
-            <GlobalQuarklyPageStyles pageUrl={"index"} />
-            <Helmet>
-                <title>Quarkly export</title>
-                <meta name={"description"} content={"Web site created using quarkly.io"} />
+  
                 <link rel={"shortcut icon"} href={"https://uploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17:34:40.476Z"} type={"image/x-icon"} />
                 <link rel={"apple-touch-icon"} href={"https://uploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17:34:40.476Z"} />
                 <link rel={"apple-touch-icon"} sizes={"76x76"} href={"https://uploads.quarkly.io/67c737959335410018cacb84/images/Pirsim%20Audio.png?v=2025-03-04T17:34:40.476Z"} />
